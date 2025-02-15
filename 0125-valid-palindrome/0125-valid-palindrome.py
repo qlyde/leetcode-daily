c9b7130131990dbe241a12c62ec965892124fde0
qlyde <jameskroeger123@gmail.com>
2025-02-15 18:33:30 +1100

    Time: 11 ms (36.72%), Space: 18.1 MB (68.92%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -6,7 +6,7 @@ class Solution:
                 l += 1
             while l < r and not s[r].isalnum():
                 r -= 1
-            if l > r or s[l].lower() != s[r].lower():
+            if s[l].lower() != s[r].lower():
                 return False
             l += 1
             r -= 1
